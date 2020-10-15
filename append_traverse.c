8676a7bee7d4b14285a359c5f62ff87ffcca709c
pawankalyanV <pavankalyan4070@gmail.com>
2020-10-15 17:55:17 +0530

    newly added

---
+++ append_traverse.c
@@ -0,0 +1,139 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct car {
+     char *name;
+     char *segment;
+     int milage;
+     float power;
+     struct car *next;
+};
+
+struct car *HEAD = NULL;
+
+void append(struct car *node)
+{
+        struct car *s1 = NULL;
+
+        s1 = HEAD;
+
+        if (s1 == NULL) {
+            HEAD = node;
+        } else {
+                while (s1->next != NULL) {
+                        s1 = s1->next;
+                }
+                s1->next = node;
+        }
+}
+
+void traverse(void)
+{
+        struct car *s1 = NULL;
+
+        s1 = HEAD;
+
+        while (s1 != NULL) {
+                printf("%s %s %d %f\n", s1->name, s1->segment, s1->milage, s1->power);
+                s1 = s1->next;
+        }
+
+}
+void add(struct car *newNode,int pos)
+{
+
+        struct car *s1 = NULL;
+
+        struct car *prev = s1;
+
+        prev = HEAD;
+
+        s1 = HEAD;
+
+        for (int i=1; i<pos; i++) {
+                prev = s1;
+                s1 = s1->next;
+        }
+        prev->next = newNode;
+        newNode->next = s1;
+}
+
+void delete(int pos)
+{
+        struct car *s1 = NULL;
+
+        struct car *prev;
+
+        prev = HEAD;
+
+        s1 = HEAD;
+
+        for(int i=1; i<pos; i++) {
+                prev = s1;
+                s1 = s1->next;
+        }
+        prev->next = s1->next;
+        free(s1);
+}
+
+int main()
+{
+        struct car *s1 = NULL;
+        s1 = (struct car *)malloc(sizeof(struct car));
+        s1->name = "Honda city";
+        s1->segment = "suv";
+        s1->milage = 20;
+        s1->power = 80;
+        s1->next = NULL;
+        append(s1);
+
+        s1 = (struct car *)malloc(sizeof(struct car));
+        s1->name = "Tata nexon";
+        s1->segment = "Hatch Back";
+        s1->milage = 24;
+        s1->power = 87;
+        s1->next = NULL;
+        append(s1);
+
+        traverse();
+
+        s1 = (struct car *)malloc(sizeof(struct car));
+        s1->name = "Honda city";
+        s1->segment = "suv";
+        s1->milage = 20;
+        s1->power = 80;
+        s1->next = NULL;
+        add(s1,2);
+        traverse();
+        delete(2);
+        traverse();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
